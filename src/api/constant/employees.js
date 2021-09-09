e99ad3214c9d79539b98ee287972f8f86ca196a5
jasmine <18674601207@163.com>
2021-09-09 19:13:49 +0800

    formatter enumType column data

---
+++ src/api/constant/employees.js
@@ -0,0 +1,10 @@
+export default {
+    hireType: [{
+        id: 1,
+        value: "正式"
+
+    }, {
+        id: 2,
+        value: "非正式"
+    }]
+}
--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -11,7 +11,7 @@
         <!-- <span slot="before">共xxx条记录</span> -->
 
         <template v-slot:before>
-          <span>共xxx条记录</span>
+          <span>共{{ page.total }}条记录</span>
         </template>
 
         <template slot="after">
@@ -20,12 +20,90 @@
           <el-button size="small" type="primary">新增员工</el-button>
         </template>
       </page-tools>
+
+      <!-- 放置表格和分页 -->
+      <el-card v-loading="loading">
+        <el-table border :data="list">
+          <el-table-column label="序号" sortable="" type="index" />
+          <el-table-column label="姓名" sortable="" prop="username" />
+          <el-table-column label="工号" sortable="" prop="workNumber" />
+          <el-table-column
+            label="聘用形式"
+            sortable=""
+            prop="formOfEmployment"
+            :formatter="formatFormOfEmployment"
+          />
+          <el-table-column label="部门" sortable="" prop="departmentName" />
+          <el-table-column label="入职时间" sortable="" prop="timeOfEntry" />
+          <el-table-column label="账户状态" sortable="" prop="enableState" />
+          <el-table-column label="操作" sortable="" fixed="right" width="280">
+            <template>
+              <el-button type="text" size="small">查看</el-button>
+              <el-button type="text" size="small">转正</el-button>
+              <el-button type="text" size="small">调岗</el-button>
+              <el-button type="text" size="small">离职</el-button>
+              <el-button type="text" size="small">角色</el-button>
+              <el-button type="text" size="small">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <!-- 分页组件 -->
+        <el-row
+          type="flex"
+          justify="center"
+          align="middle"
+          style="height: 60px"
+        >
+          <el-pagination
+            layout="prev, pager, next"
+            :page-size="page.size"
+            :current-page="page.page"
+            :total="page.total"
+            @current-change="changePage"
+          />
+        </el-row>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import { getEmployeeList } from "@/api/employees";
+import employeesEnum from "@/api/constant/employees";
+export default {
+  data() {
+    return {
+      loading: false,
+      list: [], // 接数据的
+      page: {
+        page: 1, // 当前页码
+        size: 10,
+        total: 0, // 总数
+      },
+    };
+  },
+  created() {
+    this.getEmployeeList();
+  },
+  methods: {
+    changePage(newPage) {
+      this.page.page = newPage;
+      this.getEmployeeList();
+    },
+    async getEmployeeList() {
+      this.loading = true;
+      const { total, rows } = await getEmployeeList(this.page);
+      this.page.total = total;
+      this.list = rows;
+      this.loading = false;
+    },
+    //格式化聘用形式列
+    formatFormOfEmployment(row, column, cellValue) {
+      const item = employeesEnum.hireType.find((item) => item.id === cellValue);
+      return item ? item.value : "未知";
+    },
+  },
+};
 </script>
 
 <style>
